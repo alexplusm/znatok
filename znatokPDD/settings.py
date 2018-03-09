378e173e289d31e2d1e14dcf75ded2ced3458a01
Sergey Smirnov <kurchatov1996@yandex.ru>
2018-03-09 18:10:25 +0300

    zaebalsya delat commity

--- znatokPDD/settings.py
+++ znatokPDD/settings.py
@@ -100,14 +100,11 @@ WSGI_APPLICATION = 'znatokPDD.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'znatok',
-         'USER': 'znatok',
-         'PASSWORD': '0',
-         'HOST': 'localhost',
-         'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
