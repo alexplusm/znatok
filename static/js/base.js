3e18fedd2abc0e74cd754d1a3ebf9020f53a50b9
Easterok <caji@mail.ru>
2018-08-29 17:05:21 +0300

    fix getPoints after mini-game end

--- static/js/base.js
+++ static/js/base.js
@@ -84,20 +84,8 @@ function playerAction(elem, id) {
       success: function (data) {
         if (data.bool) {
           $('.js-alert-success').addClass('game__alert_opacity-1');
-          $.ajax({
+          getPoints();
 
-            type: "GET",
-
-            url: "/check_points/",
-
-            dataType: 'json',
-
-            cache: false,
-
-            success: function (data) {
-              document.getElementById('bonus').textContent = data.points;
-            }
-          });
           setTimeout(() => {
              $.ajax({
 
@@ -221,6 +209,24 @@ function checkStageMiniGame() {
       } else {
         miniGameNotReady('starttimer');
       }
+      getPoints();
+    }
+  });
+}
+
+function getPoints() {
+  $.ajax({
+
+    type: "GET",
+
+    url: "/check_points/",
+
+    dataType: 'json',
+
+    cache: false,
+
+    success: function (data) {
+      document.getElementById('bonus').textContent = data.points;
     }
   });
 }
